a0047c1435d0fe49365ef58da186583ea192f9d3
sungjin <sungjinny5@naver.com>
2023-09-22 17:10:42 +0900

    member Entity 생성

--- src/main/java/com/pet/petproject/member/entity/Member.java
+++ src/main/java/com/pet/petproject/member/entity/Member.java
@@ -1,18 +1,15 @@
 package com.pet.petproject.member.entity;
 
-import co.elastic.clients.util.DateTime;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @Getter
