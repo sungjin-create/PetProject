33f86b0c9348b1a70f47f9f2520d45b3435b9506
sungjin <sungjinny5@naver.com>
2023-09-22 15:11:07 +0900

    security 설정

---
+++ src/main/java/com/pet/petproject/member/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.pet.petproject.member.config;
+
+import com.pet.petproject.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    protected SecurityFilterChain webSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/member/join", "/member/login", "/member/mail/**").permitAll()
+                .antMatchers("/member/test").authenticated()
+                .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
