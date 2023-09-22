c7cedf15581cd07ddbfcceecdc40510c988e7f2e
sungjin <sungjinny5@naver.com>
2023-09-22 17:13:25 +0900

    MemberService - 회원가입, 이메일 인증, 토큰 관련 서비스 구현

--- src/main/java/com/pet/petproject/member/service/MemberService.java
+++ src/main/java/com/pet/petproject/member/service/MemberService.java
@@ -26,105 +26,105 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class MemberService implements UserDetailsService {
 
-    private final MemberRepository memberRepository;
-    private final BCryptPasswordEncoder encoder;
-    private final MailComponents mailComponents;
+  private final MemberRepository memberRepository;
+  private final BCryptPasswordEncoder encoder;
+  private final MailComponents mailComponents;
 
-    @Value("${jwt.secret}")
-    private String secretKey;
+  @Value("${jwt.secret}")
+  private String secretKey;
 
-    private Long expiredMs = 1000*60*60l;
+  private Long expiredMs = 1000 * 60 * 60l;
 
-    //가입하기
-    public void join(JoinMember parameter) {
-        String name = parameter.getName();
-        String email = parameter.getEmail();
-        String password = parameter.getPassword();
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+  //가입하기
+  public void join(JoinMember parameter) {
+    String name = parameter.getName();
+    String email = parameter.getEmail();
+    String password = parameter.getPassword();
+    Optional<Member> optionalMember = memberRepository.findByEmail(email);
 
-        //이미 가입한 사람인지 체크
-        if (optionalMember.isPresent()) {
-            throw new RuntimeException(email + "는 이미 있습니다.");
-        }
-
-        String uuid = UUID.randomUUID().toString();
-        Member member = Member.builder()
-                .name(name)
-                .email(email)
-                .password(encoder.encode(password))
-                .emailAuthYn(false)
-                .emailAuthKey(uuid)
-                .registerDate(LocalDateTime.now())
-                .build();
-
-        memberRepository.save(member);
-
-        sendEmailAuth(email, uuid);
-
+    //이미 가입한 사람인지 체크
+    if (optionalMember.isPresent()) {
+      throw new RuntimeException(email + "는 이미 있습니다.");
     }
 
-    //이메일 보내기
-    private void sendEmailAuth(String email, String uuid) {
-        String subject = "PetProject에 가입을 축하드립니다.";
-        String text = "<p>PetProject에 가입을 축하드립니다.</p><p>아래 링크를 클릭하셔서 가입을 완료 하세요.</p>" +
-                "<div><a href='http://localhost:8080/member/mail/auth?id=" + uuid + "'>가입완료</a></div>";
-        mailComponents.sendMail(email, subject, text);
+    String uuid = UUID.randomUUID().toString();
+    Member member = Member.builder()
+        .name(name)
+        .email(email)
+        .password(encoder.encode(password))
+        .emailAuthYn(false)
+        .emailAuthKey(uuid)
+        .registerDate(LocalDateTime.now())
+        .build();
+
+    memberRepository.save(member);
+
+    sendEmailAuth(email, uuid);
+
+  }
+
+  //이메일 보내기
+  private void sendEmailAuth(String email, String uuid) {
+    String subject = "PetProject에 가입을 축하드립니다.";
+    String text = "<p>PetProject에 가입을 축하드립니다.</p><p>아래 링크를 클릭하셔서 가입을 완료 하세요.</p>" +
+        "<div><a href='http://localhost:8080/member/mail/auth?id=" + uuid + "'>가입완료</a></div>";
+    mailComponents.sendMail(email, subject, text);
+  }
+
+  //이메일 인증 권한 부여하기
+  public boolean emailAuthGranted(String uuid) {
+    //이메일 인증키로 체크
+    Optional<Member> opMember = memberRepository.findByEmailAuthKey(uuid);
+    if (opMember.isEmpty()) {
+      return false;
     }
 
-    //이메일 인증 권한 부여하기
-    public boolean emailAuthGranted(String uuid) {
-        //이메일 인증키로 체크
-        Optional<Member> opMember = memberRepository.findByEmailAuthKey(uuid);
-        if (opMember.isEmpty()) {
-            return false;
-        }
+    Member member = opMember.get();
+    member.setEmailAuthYn(true);
+    member.setEmailAuthDate(LocalDateTime.now());
+    memberRepository.save(member);
+    return true;
+  }
 
-        Member member = opMember.get();
-        member.setEmailAuthYn(true);
-        member.setEmailAuthDate(LocalDateTime.now());
-        memberRepository.save(member);
-        return true;
+  //유저 로드 및 권한부여
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    Member member = memberRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
+
+    List<GrantedAuthority> grantedAuthorities = new LinkedList<>();
+    grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+    return new User(member.getEmail(), member.getPassword(), grantedAuthorities);
+  }
+
+  //이메일로 회원가입한 멤버인지 체크
+  public boolean findEmail(String email) {
+    Optional<Member> optionalMember = memberRepository.findByEmail(email);
+    if (optionalMember.isEmpty()) {
+      return false;
     }
+    return true;
+  }
 
-    //유저 로드 및 권한부여
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Member member = memberRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-
-        List<GrantedAuthority> grantedAuthorities = new LinkedList<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-
-        return new User(member.getEmail(), member.getPassword(), grantedAuthorities);
+  //이메일 권한여부 체크
+  public boolean checkEmailAuth(String email) {
+    Optional<Member> opMember = memberRepository.findByEmail(email);
+    boolean emailAuthYn = opMember.get().isEmailAuthYn();
+    if (!emailAuthYn) {
+      return false;
     }
+    return true;
+  }
 
-    //이메일로 회원가입한 멤버인지 체크
-    public boolean findEmail(String email) {
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
-        if (optionalMember.isEmpty()) {
-            return false;
-        }
-        return true;
+  //패스워드 일치하는지 체크
+  public boolean checkPassword(String email, String password) {
+    Optional<Member> optionalMember = memberRepository.findByEmail(email);
+    String existPassword = optionalMember.get().getPassword();
+
+    if (!encoder.matches(password, existPassword)) {
+      return false;
     }
-
-    //이메일 권한여부 체크
-    public boolean checkEmailAuth(String email) {
-        Optional<Member> opMember = memberRepository.findByEmail(email);
-        boolean emailAuthYn = opMember.get().isEmailAuthYn();
-        if (!emailAuthYn) {
-            return false;
-        }
-        return true;
-    }
-
-    //패스워드 일치하는지 체크
-    public boolean checkPassword(String email, String password) {
-        Optional<Member> optionalMember = memberRepository.findByEmail(email);
-        String existPassword = optionalMember.get().getPassword();
-
-        if (!encoder.matches(password, existPassword)) {
-            return false;
-        }
-        return true;
-    }
+    return true;
+  }
 }
