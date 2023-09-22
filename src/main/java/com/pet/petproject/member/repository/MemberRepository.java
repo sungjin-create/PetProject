e51b94f336c1be30d098dfbf65dc56e8c3b0ae41
sungjin <sungjinny5@naver.com>
2023-09-22 17:14:46 +0900

    MemberRepository 생성

--- src/main/java/com/pet/petproject/member/repository/MemberRepository.java
+++ src/main/java/com/pet/petproject/member/repository/MemberRepository.java
@@ -6,7 +6,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
 
-    Optional<Member> findByEmailAuthKey(String emailAuthKey);
+  Optional<Member> findByEmail(String email);
+
+  Optional<Member> findByEmailAuthKey(String emailAuthKey);
 }
