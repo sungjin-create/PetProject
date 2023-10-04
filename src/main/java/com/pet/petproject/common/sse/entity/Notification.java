9cefa69d6cf1493171d1c44e3b051c16f2399f5b
sungjin <sungjinny5@naver.com>
2023-10-04 23:20:22 +0900

    feat: Notification entity, dto 구현

---
+++ src/main/java/com/pet/petproject/common/sse/entity/Notification.java
@@ -0,0 +1,41 @@
+package com.pet.petproject.common.sse.entity;
+
+import com.pet.petproject.member.entity.Member;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@EqualsAndHashCode
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Notification {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "notification_id")
+  private Long id;
+
+  @ManyToOne
+  private Member receiver;
+
+  @Enumerated(value = EnumType.STRING)
+  private NotificationType notificationType;
+
+  private String content;
+
+}
