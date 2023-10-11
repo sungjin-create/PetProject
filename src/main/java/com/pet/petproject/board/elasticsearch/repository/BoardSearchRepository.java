ae5b5731875e567b8d8ce7579f52d7735842dfc2
sungjin <sungjinny5@naver.com>
2023-10-11 12:19:45 +0900

    feat: es dto, repository 구현

---
+++ src/main/java/com/pet/petproject/board/elasticsearch/repository/BoardSearchRepository.java
@@ -0,0 +1,17 @@
+package com.pet.petproject.board.elasticsearch.repository;
+
+
+import com.pet.petproject.board.elasticsearch.entity.BoardDocument;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface BoardSearchRepository extends ElasticsearchRepository<BoardDocument, Long> {
+
+    Page<BoardDocument> findByTitle(String title, Pageable pageable);
+
+    Page<BoardDocument> findByContents(String contents, Pageable pageable);
+
+    Page<BoardDocument> findByTitleOrContents(String keyword1, String keyword2, Pageable pageable);
+
+}
